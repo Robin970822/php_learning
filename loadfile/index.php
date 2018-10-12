c2c912b0ea3829b32f2a2ab870e0b2cf30bdaa12
Robin970822 <717740827@qq.com>
2018-10-12 19:24:15 +0800

    include and require

---
+++ loadfile/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: robin_hanxy
+ * Date: 2018/10/12
+ * Time: 18:57
+ */
+
+include 'lib.php';
+
+sayHello();
---
+++ loadfile/lib.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: robin_hanxy
+ * Date: 2018/10/12
+ * Time: 18:57
+ */
+
+function sayHello()
+{
+    echo 'Hello' . '<br>';
+}
