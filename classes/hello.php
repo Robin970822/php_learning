2a96b69458b5e356cfc68d36a837bd0853a69108
Robin970822 <717740827@qq.com>
2018-10-13 15:40:41 +0800

    class

---
+++ classes/hello.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: robin_hanxy
+ * Date: 2018/10/12
+ * Time: 19:24
+ */
+
+class Hello
+{
+    public function sayHello()
+    {
+        echo 'Hello';
+    }
+}
---
+++ classes/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: robin_hanxy
+ * Date: 2018/10/12
+ * Time: 19:24
+ */
+
+require_once 'hello.php';
+
+$h = new Hello();
+
+$h -> sayHello();
